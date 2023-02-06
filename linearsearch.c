1737e1ce03f0246f4f53fad6d088375af0c011ac
TanishGupta09 <124549370+TanishGupta09@users.noreply.github.com>
2023-02-06 10:43:24 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100];
+    int size,i,search,found=0;
+
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nEnter element to search: ");
+    scanf("%d", &search);
+
+    for(i=0; i<size; i++)
+    {
+        if(arr[i] == search)
+        {
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 1)
+    {
+        printf("\n%d is found at position %d",search, i + 1);
+    }
+    else
+    {
+        printf("\n%d is not found in the array",search);
+    }
+
+    return 0;
+}
